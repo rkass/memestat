5746ddfd759eae0d402406b5eb4d784d5b865ea5
rkass <rck72@cornell.edu>
2013-01-25 12:55:51 -0500

    X

---
+++ memestat/testing/meme_count.py
@@ -0,0 +1,11 @@
+import sys
+import os
+from django.core.management import setup_environ
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import settings
+setup_environ(settings)
+from stats.models import ImageMacro
+from stats.models import Meme
+from stats.models import PotentialImageMacro
+
+print Meme.object.all().count()
