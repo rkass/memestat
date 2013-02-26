ac5df112d4503ec076f278f1a2f68a7a16d9240b
rkass <rck72@cornell.edu>
2013-02-25 21:18:23 -0500

    x

--- memestat/settings.py
+++ memestat/settings.py
@@ -56,7 +56,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/')
+
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -66,7 +67,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    # Don't forget to use absolute paths, not relative pathsi.
+  '/home/ryan/Programming/OpenCV-2.4.2/msheroku/memestat/stats/static'
 )
 
 # List of finder classes that know how to find static files in
