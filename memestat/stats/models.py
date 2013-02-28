52f8929391467c206a6870242e340a448adf97b7
rkass <rck72@cornell.edu>
2013-02-27 21:24:32 -0500

    X

--- memestat/stats/models.py
+++ memestat/stats/models.py
@@ -1,12 +1,52 @@
 from django.db import models
+from django.utils.timezone import utc
+from datetime import datetime, timedelta
+from operator import itemgetter
 
-#class MacroGrouper(models.Manager):
- # def group(self):
-    
+#Takes a single macro group along with time constraints and outputs
+#the score for that macro group for the given time constraints 
+def tallyScore(group, 
+  lowerLimit = datetime(2012, 5, 3, 17, 56, 17, 828221, tzinfo=utc),
+  upperLimit = datetime.now().replace(tzinfo = utc)):
+  score = 0
+  for macro in group:
+    memes = Meme.objects.filter(classification = macro, created_at__gte = lowerLimit, 
+      created_at__lte = upperLimit)
+    for meme in memes:
+      score += meme.score
+  return score
+#Takes an entire set of macro groups along with time constraints and
+#outputs an array of two-tuples of group, score sorted by the tuples
+#with highest score first
+def tallyScoresGroups(groups, 
+  lowerLimit = datetime(2012, 5, 3, 17, 56, 17, 828221, tzinfo=utc),
+  upperLimit = datetime.now().replace(tzinfo = utc)):
+  returnSet = []
+  for group in groups:
+    returnSet.append((group, tallyScore(group, lowerLimit, upperLimit)))
+  return sorted(returnSet, key = lambda tup: tup[1])[::-1] 
+ 
+class MacroGrouper(models.Manager):
+  def group(self):
+    allMacros = self.all().order_by('name')
+    lastName = ''
+    returnSet = []
+    for macro in allMacros:
+      if macro.name == '' or lastName != macro.name:
+        returnSet.append([macro])
+      else:
+        returnSet[-1].append(macro)
+      lastName = macro.name
+    return returnSet
+  #returns the highest scoring memegroup for the last 24 hours along with score in a tuple
+  def memeOfTheDay(self):
+    return tallyScoresGroups(self.group(), 
+      lowerLimit = datetime.utcnow().replace(tzinfo = utc) - timedelta(days = 1))[0]
 
 class ImageMacro(models.Model):
   key = models.CharField(max_length = 1000)
   name = models.CharField(max_length = 1000, null = True)
+  objects = MacroGrouper()
 ##For the next two models, created refers to thread creation,
 ##and created_at refers to object creation
 
@@ -36,26 +76,27 @@ class PotentialImageMacro(models.Model):
   created_at = models.DateTimeField(auto_now_add = True)
   active = models.BooleanField(default = True)
 
+#changes are percent values rounded to the nearest integer
 class TopMacro(models.Model):
   macros = models.ManyToManyField(ImageMacro)
   dailyScore = models.IntegerField()
   hourlyScore = models.IntegerField()
-  dailyChange = models.IntegerField()
-  hourlyChange = models.IntegerField()
+  dailyChange = models.IntegerField(null = True)
+  hourlyChange = models.IntegerField(null = True)
   created_at = models.DateTimeField(auto_now_add = True)
 
 class ShootingStar(models.Model):
   macros = models.ManyToManyField(ImageMacro)
   dailyScore = models.IntegerField()
   hourlyScore = models.IntegerField()
-  dailyChange = models.IntegerField()
-  hourlyChange = models.IntegerField()
+  dailyChange = models.IntegerField(null = True)
+  hourlyChange = models.IntegerField(null = True)
   created_at = models.DateTimeField(auto_now_add = True)
 
 class SinkingStone(models.Model):
   macros = models.ManyToManyField(ImageMacro)
   dailyScore = models.IntegerField()
   hourlyScore = models.IntegerField()
-  dailyChange = models.IntegerField()
-  hourlyChange = models.IntegerField()
+  dailyChange = models.IntegerField(null = True)
+  hourlyChange = models.IntegerField(null = True)
   created_at = models.DateTimeField(auto_now_add = True)
