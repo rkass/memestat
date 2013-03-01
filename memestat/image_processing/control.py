d5644ac341a48190a564f39611c22443daf7e669
rkass <rck72@cornell.edu>
2013-03-01 18:16:37 -0500

    x

--- memestat/image_processing/control.py
+++ memestat/image_processing/control.py
@@ -52,7 +52,7 @@ def updateName(macro):
   if inCommon == 0:
     memes = Meme.objects.filter(classification = macro).distinct().order_by('topDist')
     for m in memes:
-      if m.name != '' and m.meme != None:
+      if m.name != '' and m.name != None:
         macro.name = m.name
         macro.save()
         return m.name
