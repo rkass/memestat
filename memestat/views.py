68139134496c46ea37ceb70d8080bb6f21158d82
rkass <rck72@cornell.edu>
2013-03-05 21:16:57 -0500

    x

--- memestat/views.py
+++ memestat/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from stats.models import *
 
+def orderMemes(macroGroup):
+  ret = Meme.objects.filter(classification = macroGroup[0])
+  for mac in macroGroup:
+    ret |= Meme.objects.filter(classification = mac)
+  return ret.order_by('topDist')
+
 def home(request):
   topMeme = TopMacro.objects.latest('id')
-  sinkingStone = TopMacro.objects.latest('id')
-  shootingStar = TopMacro.objects.latest('id')
-  return render_to_response('home.html', {'shootingStar' : shootingStar}, context_instance=RequestContext(request))
+  sinkingStone = SinkingStone.objects.latest('id')
+  shootingStar = ShootingStar.objects.latest('id')
+  topMemeImg = orderMemes(topMeme.macros.all())[0].fullSizeLink
+  sinkingStoneImg = orderMemes(sinkingStone.macros.all())[0].fullSizeLink
+  shootingStarImg = orderMemes(shootingStar.macros.all())[0].fullSizeLink
+  
+  return render_to_response('home.html', {'topMemeImg' : topMemeImg, 
+                                          'shootingStarImg' : shootingStarImg,
+                                          'sinkingStoneImg' : sinkingStoneImg,
+                                          'topMemeUvt' : topMeme.dailyScore,
+                                          'topMemeDc' : topMeme.dailyChange,
+                                          'shootingStarUvt' : shootingStar.dailyScore,
+                                          'shootingStarDc' : shootingStar.dailyChange,
+                                          'sinkingStoneUvt' : sinkingStone.dailyScore,
+                                          'sinkingStoneDc' : sinkingStone.dailyChange}, context_instance=RequestContext(request))
